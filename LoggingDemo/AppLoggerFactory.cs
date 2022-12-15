53c33f3396a24137032d3288b15b563bc5278c50
Michal Korniak <michal.korniak@kornit.com>
2022-12-15 13:08:26 +0100

    Added Elastic and Kibana

---
+++ LoggingDemo/AppLoggerFactory.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Elasticsearch;
+using MS = Microsoft.Extensions.Logging;
+
+namespace ConsoleWithLogger
+{
+    internal class AppLoggerFactory : IDisposable
+    {
+
+        private readonly Logger _serilogLogger;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public AppLoggerFactory()
+        {
+            string appName = "Demo";
+            string envName = "Local";
+            string elasticSearchUrl = "http://localhost:9200";
+            string seqUrl = "http://localhost:5341/";
+
+            _serilogLogger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithSpan()
+                    .Enrich.WithExceptionDetails()
+                    .WriteTo.Console(new CompactJsonFormatter())
+                    .WriteTo.Seq(seqUrl)
+                    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticSearchUrl))
+                    {
+                        IndexFormat = $"{appName}-{envName}-{DateTimeOffset.Now:yyyy-MM}",
+                        AutoRegisterTemplate = true,
+                        OverwriteTemplate = true,
+                        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                        TypeName = null,
+                        BatchAction = ElasticOpType.Create,
+                        InlineFields = true,
+                    })
+                    .CreateLogger();
+
+            _loggerFactory = MS.LoggerFactory.Create(builder =>
+            {
+                builder.AddSerilog(_serilogLogger);
+            });
+        }
+
+        public MS.ILogger CreateLogger(string categoryName)
+        {
+            return _loggerFactory.CreateLogger(categoryName);
+        }
+
+        public MS.ILogger CreateLogger<T>()
+        {
+            return _loggerFactory.CreateLogger<T>();
+        }
+
+        public void Dispose()
+        {
+            _serilogLogger?.Dispose();
+            _loggerFactory?.Dispose();
+        }
+    }
+}
---
+++ LoggingDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleWithLogger;
+using Microsoft.Extensions.Logging;
+using Serilog.Core;
+using System.Diagnostics;
+
+public class Program
+{
+    public static void Main()
+    {
+        using (var loggerFactory = new AppLoggerFactory())
+        {
+            ProduceSomeLogs(loggerFactory.CreateLogger("StructuredLogs"), "Ben");
+            loggerFactory.CreateLogger("Main").LogInformation("In main");
+        }
+    }
+
+    public static void ProduceSomeLogs(ILogger logger, string userName)
+    {
+        using var mainActivity = new Activity("main-mainActivity");
+        mainActivity.Start();
+
+        logger.LogInformation("User {userName} registered at {registeredDate}", userName, DateTime.UtcNow);
+
+        Guid orderId = Guid.NewGuid();
+        logger.LogInformation("User {userName} created new order {orderId}", userName, orderId);
+        logger.LogInformation("Order {orderId} shipped", orderId);
+        logger.LogInformation("Order {orderId} finished", orderId);
+
+        Guid secondOrderId = Guid.NewGuid();
+        logger.LogInformation("User {userName} created new order {orderId}", userName, secondOrderId);
+        logger.LogInformation("Order {orderId} canceled", secondOrderId);
+
+        try
+        {
+            int y = 0;
+            int x = 100 / y;
+        }
+        catch (Exception ex)
+        {
+            using var errorHandlingActivity = new Activity("error-handling");
+            errorHandlingActivity.SetParentId(Activity.Current.Id);
+            errorHandlingActivity.Start();
+            ex.Data.Add("xx", "yy");
+            logger.LogError(new ArgumentException("eee", ex), "Something's wrong.");
+        }
+        logger.LogInformation("Processing finished");
+    }
+
+}
